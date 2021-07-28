fff32667cfc1ef6b62ee31ff0a7d37f00f403031
Sohail Arshed <rmsohaila@gmail.com>
2021-07-28 07:59:29 +0500

    card-component inputs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
+import { CardComponent } from './card/card.component';
 import { DetailComponent } from './detail/detail.component';
 import { IndexComponent } from './index/index.component';
 import { NgModule } from '@angular/core';
-import { CardComponent } from './card/card.component';
 
 @NgModule({
-  declarations: [AppComponent, IndexComponent, DetailComponent, CardComponent],
+  declarations: [AppComponent, CardComponent, IndexComponent, DetailComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'git-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
 })
 export class CardComponent implements OnInit {
+  @Input('avatar')
+  _avatar!: String;
+  @Input('url')
+  _url!: String;
+  @Input('name')
+  _name!: String;
+  @Input('description')
+  _description!: String;
+  @Input('stars')
+  _stars!: string;
+  @Input('issues')
+  _issues!: string;
+  @Input('author')
+  _author!: String;
 
-  constructor() { }
+  public avatar: String;
+  public url: String;
+  public name: String;
+  public description: String;
+  public stars: string;
+  public issues: string;
+  public author: String;
 
-  ngOnInit(): void {
+  constructor() {
+    this.avatar = this._avatar;
+    this.url = this._url;
+    this.name = this._name;
+    this.description = this._description;
+    this.stars = this._stars;
+    this.issues = this._issues;
+    this.author = this._author;
+
+    console.log(this._author);
   }
 
+  ngOnInit(): void {}
 }
